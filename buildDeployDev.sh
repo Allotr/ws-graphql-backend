48e1e86af667ebae3c6c07408e8e6d6f6144eee2
Rafael Pernil <rafaelpernil@uma.es>
2021-08-26 00:20:40 +0200

    feat: Adapted for Dev and Prod

--- buildDeployDev.sh
+++ buildDeployDev.sh
@@ -3,8 +3,8 @@ docker build -t allotr-ws-graphql-backend .
 docker tag allotr-ws-graphql-backend rafaelpernil/allotr-ws-graphql-backend
 docker push rafaelpernil/allotr-ws-graphql-backend
 
-kubectl apply -f ./artifacts/deployment.yaml
-kubectl apply -f ./artifacts/service.yaml
+kubectl apply -f ./artifacts/dev/deployment.yaml
+kubectl apply -f ./artifacts/dev/service.yaml
 
 kubectl scale --replicas=0 deployment allotr-ws-graphql-backend -n openfaas-fn
 kubectl scale --replicas=2 deployment allotr-ws-graphql-backend -n openfaas-fn
---
+++ buildDeployProd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker build -t allotr-ws-graphql-backend .
+docker tag allotr-ws-graphql-backend rafaelpernil/allotr-ws-graphql-backend:production
+docker push rafaelpernil/allotr-ws-graphql-backend:production
+
+kubectl apply -f ./artifacts/prod/deployment.yaml
+kubectl apply -f ./artifacts/prod/service.yaml
+
+kubectl scale --replicas=0 deployment allotr-ws-graphql-backend -n openfaas-fn
+kubectl scale --replicas=2 deployment allotr-ws-graphql-backend -n openfaas-fn
