1c4010e9c3ecefb031d922ecd27752979bb1129a
rafaelpernil2 <mysterysynthman@gmail.com>
2023-08-13 00:39:24 +0200

    fix: .env to secret

--- buildDeploy.sh
+++ buildDeploy.sh
@@ -3,8 +3,8 @@ docker build -t allotr-ws-graphql-backend .
 docker tag allotr-ws-graphql-backend rafaelpernil/allotr-ws-graphql-backend:production
 docker push rafaelpernil/allotr-ws-graphql-backend:production
 
-kubectl apply -f ./artifacts/prod/deployment.yaml
-kubectl apply -f ./artifacts/prod/service.yaml
+kubectl apply -f ./artifacts/deployment.yaml
+kubectl apply -f ./artifacts/service.yaml
 
 kubectl scale --replicas=0 deployment allotr-ws-graphql-backend -n openfaas-fn
 kubectl scale --replicas=2 deployment allotr-ws-graphql-backend -n openfaas-fn
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-kubectl apply -f ./artifacts/dev/deployment.yaml
-kubectl apply -f ./artifacts/dev/service.yaml
+kubectl apply -f ./artifacts/deployment.yaml
+kubectl apply -f ./artifacts/service.yaml
 
 kubectl scale --replicas=0 deployment allotr-ws-graphql-backend -n openfaas-fn
 kubectl scale --replicas=2 deployment allotr-ws-graphql-backend -n openfaas-fn
