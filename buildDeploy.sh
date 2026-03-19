db69fe408b2cc3ecab8869b434d7daba7f50b0a4
rafelpernil2 <mysterysynthman@gmail.com>
2026-03-19 18:11:26 +0100

    feat: Remove OpenFaas because new Community Edition plan breaks the application

--- buildDeploy.sh
+++ buildDeploy.sh
@@ -6,5 +6,5 @@ docker push rafaelpernil/allotr-ws-graphql-backend:latest
 kubectl apply -f ./artifacts/deployment.yaml
 kubectl apply -f ./artifacts/service.yaml
 
-kubectl scale --replicas=0 deployment allotr-ws-graphql-backend -n openfaas-fn
-kubectl scale --replicas=2 deployment allotr-ws-graphql-backend -n openfaas-fn
+kubectl scale --replicas=0 deployment allotr-ws-graphql-backend -n allotr
+kubectl scale --replicas=2 deployment allotr-ws-graphql-backend -n allotr
--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 kubectl apply -f ./artifacts/deployment.yaml
 kubectl apply -f ./artifacts/service.yaml
 
-kubectl scale --replicas=0 deployment allotr-ws-graphql-backend -n openfaas-fn
-kubectl scale --replicas=2 deployment allotr-ws-graphql-backend -n openfaas-fn
+kubectl scale --replicas=0 deployment allotr-ws-graphql-backend -n allotr
+kubectl scale --replicas=2 deployment allotr-ws-graphql-backend -n allotr
